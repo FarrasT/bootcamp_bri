549c3a90d2e893106e14651eeaf24b3f759f2428
Farras Timorremboko <farras.timorremboko@corp.bri.co.id>
2024-02-13 10:58:17 +0700

    create login

--- golang_jwt/controllers/userControllers.go
+++ golang_jwt/controllers/userControllers.go
@@ -39,3 +39,43 @@ func UserRegister(c *gin.Context) {
 		"full_name": User.FullName,
 	})
 }
+
+func UserLogin(c *gin.Context) {
+	db := database.GetDB()
+	contentType := helpers.GetContentType(c)
+	_, _ = db, contentType
+	User := models.User{}
+	password := ""
+
+	if contentType == appJson {
+		c.ShouldBindJSON(&User)
+	} else {
+		c.ShouldBind(&User)
+	}
+
+	password = User.Password
+
+	if err := db.Debug().Where("email = ?", User.Email).Take(&User).Error; err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"error":   "Unauthorized",
+			"message": "invalid email/password",
+		})
+		return
+	}
+
+	comparePass := helpers.ComparePass([]byte(User.Password), []byte(password))
+
+	if !comparePass {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"error":   "Unauthorized",
+			"message": "invalid email/password",
+		})
+		return
+	}
+
+	token := helpers.GenerateToken(User.ID, User.Email)
+
+	c.JSON(http.StatusOK, gin.H{
+		"token": token,
+	})
+}
